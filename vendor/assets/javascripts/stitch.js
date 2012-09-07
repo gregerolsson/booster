314fba564a46fb620b277f1d64964a3b05661682
Greger <greger.olsson@redstone.se>
2012-09-07 15:59:22 +0200

    Stitch-fix, global model events

--- vendor/assets/javascripts/stitch.js
+++ vendor/assets/javascripts/stitch.js
@@ -2,52 +2,63 @@
   if (!this.require) {
     var modules = {},
         cache = {},
-        require = function(name, root) {
-          var module = cache[name], path = expand(root, name), fn;
-          if (module) {
-            return module;
-          } else if (fn = modules[path] || modules[path = expand(path, './index')]) {
-            module = {id: name, exports: {}};
-            try {
-              cache[name] = module.exports;
+        require,
+        expand,
+        dirname;
 
-              fn(module.exports, function(name) {
-                return require(name, dirname(path));
-              }, module);
+    require = function(name, root) {
+      var path = expand(root, name),
+          module = cache[path],
+          fn;
 
-              return cache[name] = module.exports;
-            } catch (err) {
-              delete cache[name];
-              throw err;
-            }
-          } else {
-            throw 'module "' + name + '" not found';
-          }
-        },
-        expand = function(root, name) {
-          var results = [], parts, part;
-          if (/^\.\.?(\/|$)/.test(name)) {
-            parts = [root, name].join('/').split('/');
-          } else {
-            parts = name.split('/');
-          }
-          for (var i = 0, length = parts.length; i < length; i++) {
-            part = parts[i];
-            if (part == '..') {
-              results.pop();
-            } else if (part != '.' && part != '') {
-              results.push(part);
-            }
-          }
-          return results.join('/');
-        },
-        dirname = function(path) {
-          return path.split('/').slice(0, -1).join('/');
-        };
+      if (module) {
+        return module;
+      } else if (fn = modules[path] || modules[path = expand(path, './index')]) {
+        module = {id: path, exports: {}};
 
-      this.require = function(name) {
-        return require(name, '');
+        try {
+          cache[path] = module.exports;
+
+          fn(module.exports, function(name) {
+            return require(name, dirname(path));
+          }, module);
+
+          return cache[path] = module.exports;
+        } catch (err) {
+          delete cache[path];
+          throw err;
+        }
+      } else {
+        throw 'module "' + path + '" not found';
       }
+    };
+
+    expand = function(root, name) {
+      var results = [], parts, part;
+
+      if (/^\.\.?(\/|$)/.test(name)) {
+        parts = [root, name].join('/').split('/');
+      } else {
+        parts = name.split('/');
+      }
+      for (var i = 0, length = parts.length; i < length; i++) {
+        part = parts[i];
+        if (part == '..') {
+          results.pop();
+        } else if (part != '.' && part != '') {
+          results.push(part);
+        }
+      }
+      return results.join('/');
+    };
+
+    dirname = function(path) {
+      return path.split('/').slice(0, -1).join('/');
+    };
+
+    this.require = function(name) {
+      return require(name, '');
+    }
 
     this.require.define = function(bundle) {
       for (var key in bundle)
